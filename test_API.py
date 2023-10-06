1218aaad666cc2403e6925d415a0afff8ed2e998
Maksim Musuleznyy <mycya2010@gmail.com>
2023-10-06 21:22:07 +0300

    Add file

---
+++ test_API.py
@@ -0,0 +1,14 @@
+# Musuleznyy Maksim
+from conftest import browser
+from testpage import OperationsHelper
+
+def test_step1():
+    logging.info("Test1 API run")
+    testpage = OperationsHelper(browser)
+    assert testpage.auth_site()[2] == testpage.check_auth(), "Test1 API failed"
+
+
+def test_step2():
+    logging.info("Test2 API run")
+    testpage = OperationsHelper(browser)
+    assert testpage.create_post() == "my_description", "Test2 API failed"
